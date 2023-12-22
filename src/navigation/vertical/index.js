bd02b8f4ba93cfc0686237a7b2d680d780395813
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-12-22 20:12:23 +0100

    fix

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -5,6 +5,7 @@ import CogOutline from 'mdi-material-ui/CogOutline'
 import CategoryRoundedIcon from '@mui/icons-material/CategoryRounded'
 import ListAltRoundedIcon from '@mui/icons-material/ListAltRounded'
 import LiveHelpRoundedIcon from '@mui/icons-material/LiveHelpRounded'
+import AddIcon from '@mui/icons-material/Add';
 
 const navigation = () => {
   
@@ -22,6 +23,11 @@ const navigation = () => {
       icon: CategoryRoundedIcon,
       title: 'Products',
       path: '/products'
+    },
+    {
+      icon: AddIcon,
+      title: 'Add product',
+      path: '/add-new-product'
     },
     {
       icon: ListAltRoundedIcon,
