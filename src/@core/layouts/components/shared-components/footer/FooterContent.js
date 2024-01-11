ffbef7d46bc52ccbb87de6f8f7529a0bdc1ef762
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-01-11 13:25:18 +0100

    fix

--- src/@core/layouts/components/shared-components/footer/FooterContent.js
+++ src/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -10,7 +10,7 @@ const FooterContent = () => {
 
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-      <Typography sx={{ mr: 2 }}>{`© ${new Date().getFullYear()}, SYNCSTORE `}</Typography>
+      <Typography sx={{ mr: 2 }}>{`© ${new Date().getFullYear()}, CRAAFT `}</Typography>
       {hidden ? null : (
         <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
           <Link
