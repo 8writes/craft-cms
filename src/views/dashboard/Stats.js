defe5feb6652005565642c83a62aedf342938412
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-01-19 13:20:49 +0100

    fix

--- src/views/dashboard/Stats.js
+++ src/views/dashboard/Stats.js
@@ -1,24 +1,69 @@
-import { useUser } from 'src/@core/context/userDataContext'
-
-// ** MUI Imports
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
-
-// ** Icons Imports
-import TrendingUp from 'mdi-material-ui/TrendingUp'
-import TourOutlinedIcon from '@mui/icons-material/TourOutlined'
-import EmojiPeopleOutlinedIcon from '@mui/icons-material/EmojiPeopleOutlined'
-import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined'
-import { Skeleton } from '@mui/material'
-
+import React, { useEffect, useState } from 'react';
+import { useUser } from 'src/@core/context/userDataContext';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+import TrendingUp from 'mdi-material-ui/TrendingUp';
+import TourOutlinedIcon from '@mui/icons-material/TourOutlined';
+import EmojiPeopleOutlinedIcon from '@mui/icons-material/EmojiPeopleOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import { Skeleton } from '@mui/material';
 
 const Stats = () => {
-  const userData = useUser()
+  const userData = useUser();
+  const [dataCount, setDataCount] = useState(0);
+   const [orderCount, setOrderCount] = useState(0);
 
- // Loading skeleton
+  const store_name_id = userData?.store_name_id;
+  const store_order_id = userData?.store_order_id
+
+  console.log(store_name_id)
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`https://craftserver.onrender.com/v1/api/fetch?store_name_id=${store_name_id}`);
+      const { error, data } = response.data;
+
+      if (error) {
+        console.error('Error fetching data:', error.message);
+
+      } else {
+        const productCount = data ? data.length : 0;
+        setDataCount(productCount);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
+
+   const fetchOrder = async () => {
+    try {
+      const response = await axios.get(` https://craftserver.onrender.com/v1/api/fetch?store_order_id=${store_order_id}`);
+      const { error, data } = response.data;
+
+      if (error) {
+        console.error('Error fetching data:', error.message);
+
+      } else {
+        const productCount = data ? data.length : 0;
+        setOrderCount(productCount);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!dataCount) {
+      fetchData();
+      fetchOrder()
+    }
+  }, [store_name_id]);
+
+  // Loading skeleton
   if (!userData) {
     return (
       <Card>
@@ -26,9 +71,9 @@ const Stats = () => {
           <Skeleton variant='rectangular' width='100%' height={150} animation='wave' />
         </CardContent>
       </Card>
-    )
+    );
   }
-  
+
   return (
     <Card>
       <CardHeader
@@ -56,7 +101,7 @@ const Stats = () => {
               <Typography variant='body2'>
               Orders
               <br />
-              <span className='text-xl'> 0 </span>{' '}
+              <span className='text-xl'> {orderCount} </span>{' '}
             </Typography>
             </span>
           </div>
@@ -74,9 +119,9 @@ const Stats = () => {
             <Inventory2OutlinedIcon sx={{ width: '25px', height: '50px' }} />
             <span className='flex-col justify-center w-full'>
               <Typography variant='body2'>
-              Products sold
+             Total Products
               <br />
-              <span className='text-xl'> 0 </span>{' '}
+              <span className='text-xl'> {dataCount} </span>{' '}
             </Typography>
             </span>
           </div>
